291c67143db40791af89eb8e06d2258befde0418
Ian Nowicki <iannowicki@Ians-MacBook-Pro.local>
2018-01-23 19:41:42 -0700

    v2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,6 @@ var wins = 0;
 var losses = 0;
 var compNumber = 0;
 var arrayRandom = [];
-var total = 0;
 
 function compNum(){
   var randomValue = Math.floor((Math.random() * 10) + 1);
@@ -16,15 +15,8 @@ for(i=0; i<5; i++) {
 for(i=0; i<4; i++) {
    var current = document.getElementById(`crystal-${i}`);
    current.innerHTML = `${arrayRandom[i]}`;
-   current.style.font-size = 0;
-
+   //current.style.font-size = 0;
+   console.log(current)
 }
 
-document.addEventListener('click', function(e) {
-    e = e || window.event;
-    var target = e.target || e.srcElement;
-    var text = target.textContent || text.innerText;  
-    total += text;
-    console.log(total);
-}, false);
-
+document.getElementById("winsLosses").innerHTML = "Erick"
